bf7db911c526dbfaa56d8e1c8824614c7c2ef759
Scot Murphy <scot@scoizzle.com>
2018-07-29 15:32:33 -0500

    Added additional test methods for debugging cryptography

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,6 +7,13 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            TestDecode();
+        }
+
+        static void Print(bool success) =>
+            Console.WriteLine(success ? "Yay!" : "Boo!");
+
+        static void Test() {
             Packet packet = new Packet();
 
             packet.SetPacketType(0x65);
@@ -33,7 +40,16 @@ namespace Test
             Print(packet.GetString() == "text");
         }
 
-        static void Print(bool success) =>
-            Console.WriteLine(success ? "Yay!" : "Boo!");
+        static void TestDecode() {
+            var buffer = new byte[] { 0x0A, 0x00, 0x1A, 0x00, 0x48, 0x2B, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x12, 0x00, 0x00, 0x00, 0x31, 0x64, 0x18 };
+
+            var packet = new Packet(buffer);
+            var uid_server_low = packet.GetUInt();
+            var uid_client_high = packet.GetUInt();
+            var uid_client_low = packet.GetUInt();
+            var time_stamp = packet.GetUInt();
+
+            Packet.MadeSeedKey(new MUID(0, uid_server_low), new MUID(uid_client_high, uid_client_low), time_stamp);
+        }
     }
 }
